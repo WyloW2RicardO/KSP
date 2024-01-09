eee545f08d59904f102af6ac0cdff2da0f81b2a4
WyloW2RicardO <wrichard@live.fr>
2024-01-09 18:25:03 +0100

    créations
    création d'un fichier python pour les convesion de données

---
+++ donnees/Conversion.py
@@ -0,0 +1,7 @@
+def PA_atm(nombre)       : return nombre / 101325
+def ergol_u_kg(nombre)   : return nombre * 5
+def oxidizer_u_kg(nombre): return nombre * 5
+def solid_u_kg(nombre)   : return nombre * 2810 / 375
+def ergol_u_c(nombre)    : return nombre * 0.8
+def oxidizer_u_c(nombre) : return nombre * 0.18
+def solid_u_c(nombre)    : return nombre * 225 / 375
