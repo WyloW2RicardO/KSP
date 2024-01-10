690d40069b672610e773b3ac04ba16bbbff7fbe4
WyloW2RicardO <wrichard@live.fr>
2024-01-10 13:17:57 +0100

    forme
    mise en forme des données

--- donnees/README.md
+++ donnees/README.md
@@ -6,7 +6,7 @@
 ### Variable
 Definition des données.
 |PARAMETRE|DEFINISION|
-|-|-|
+|:-:|-|
 |ATM|Limite atmospherique de l'objet (m)|
 |COUT|Cout de l'objet ($)|
 |DAY|Duré pour une revolution autour d'elle meme (s)|
@@ -15,14 +15,14 @@ Definition des données.
 |HAUT|Hauteur de l'objet (m)|
 |ISP_ASL|Impulsion spécifique a l'altidude 0 (s)|
 |ISP_VAC|Impulsion spécifique dans l'espace (s)|
-|KLV|Temperature moyenne de l'objet (K)|
+|KLVN|Temperature moyenne de l'objet (K)|
 |MLTPL||
 |MOL|Masse molaire moyenne de l'objet (kg.mol-1)|
 |MSS|Masse de l'objet (kg)|
 |NOM|Nom de l'objet|
 |OXDZR|Oxigenne liqude (u)|
 |OXDZR_D|Debit de l'oxigenne liquide (u.s-1)|
-|PRS|Pression atmospherique a la surface de l'objet (kg⋅m−1⋅s−2)|
-|PSS_ASL|Pousser du reacteur a l'altidude 0 (kg.m.s-2)|
-|PSS_VAC|Pousser du reacteur dans l'espace (kg.m.s-2)|
-|TLL|Diametre de l'objet (m)|
+|PRSSN|Pression atmospherique a la surface de l'objet (kg⋅m−1⋅s−2)|
+|PSSR_ASL|Pousser du reacteur a l'altidude 0 (kg.m.s-2)|
+|PSSR_VAC|Pousser du reacteur dans l'espace (kg.m.s-2)|
+|DMTR|Diametre de l'objet (m)|
--- metiers/donnee.py
+++ metiers/donnee.py
@@ -1,42 +1,42 @@
-import pandas as pd
+import pandas as PD
 
-def lire(data,ligne,colone) :
+def LIRE(data,ligne,colone) :
     if colone in data.columns.values.tolist() :
         return data.loc[ligne, [colone]].values[0][0]
     else :
         return None
 
-class lire_donnee :
+class LIRE_DONNEE :
     def __init__(self, datas, noms) :
-        url='E:/Program/KSP/DONNEES/' + datas + '.csv'
+        url='E:/Program/KSP/Donnee/' + datas + '.csv'
         try :
-            pd.read_csv(url)
+            PD.read_csv(url)
         except FileNotFoundError:
-            print('dans lire_donnee :', datas, 'est mal ecrit')
+            print('dans LIRE_DONNEE :', datas, 'est mal ecrit')
             exit()
         else :
-            self.data = pd.read_csv(url)
+            self.data = PD.read_csv(url)
         self.nom=noms
         self.lgn = self.data['NOM'].isin([self.nom])
         self.est_dans = self.lgn.any()
         if not self.est_dans:
-            print('dans lire_donnee :', self.nom, "n'est pas dans", self.data)
+            print('dans LIRE_DONNEE :', self.nom, "n'est pas dans", self.data)
             exit()
-        self.atm    =lire(self.data,self.lgn,'ATM')
-        self.cout   =lire(self.data,self.lgn,'COUT')
-        self.day    =lire(self.data,self.lgn,'DAY')
-        self.ergl   =lire(self.data,self.lgn,'ERGL')
-        self.ergl_d =lire(self.data,self.lgn,'ERGL_D')
-        self.haut   =lire(self.data,self.lgn,'HAUT')
-        self.isp_asl=lire(self.data,self.lgn,'ISP_ASL')
-        self.isp_vac=lire(self.data,self.lgn,'ISP_VAC')
-        self.klv    =lire(self.data,self.lgn,'KLV')
-        self.mltpl  =lire(self.data,self.lgn,'MLTPL')
-        self.mol    =lire(self.data,self.lgn,'MOL')
-        self.mss    =lire(self.data,self.lgn,'MSS')
-        self.oxdzr  =lire(self.data,self.lgn,'OXDZR')
-        self.oxdzr_d=lire(self.data,self.lgn,'OXDZR_D')
-        self.prs    =lire(self.data,self.lgn,'PRS')
-        self.pss_asl=lire(self.data,self.lgn,'PSS_ASL')
-        self.pss_vac=lire(self.data,self.lgn,'PSS_VAC')
-        self.tll    =lire(self.data,self.lgn,'TLL')
+        self.atm    =LIRE(self.data,self.lgn,'ATM')
+        self.cout   =LIRE(self.data,self.lgn,'COUT')
+        self.day    =LIRE(self.data,self.lgn,'DAY')
+        self.ergl   =LIRE(self.data,self.lgn,'ERGL')
+        self.ergl_d =LIRE(self.data,self.lgn,'ERGL_D')
+        self.haut   =LIRE(self.data,self.lgn,'HAUT')
+        self.isp_asl=LIRE(self.data,self.lgn,'ISP_ASL')
+        self.isp_vac=LIRE(self.data,self.lgn,'ISP_VAC')
+        self.klv    =LIRE(self.data,self.lgn,'KLV')
+        self.mltpl  =LIRE(self.data,self.lgn,'MLTPL')
+        self.mol    =LIRE(self.data,self.lgn,'MOL')
+        self.mss    =LIRE(self.data,self.lgn,'MSS')
+        self.oxdzr  =LIRE(self.data,self.lgn,'OXDZR')
+        self.oxdzr_d=LIRE(self.data,self.lgn,'OXDZR_D')
+        self.PRSSN    =LIRE(self.data,self.lgn,'PRSSN')
+        self.PSSR_ASL=LIRE(self.data,self.lgn,'PSSR_ASL')
+        self.PSSR_VAC=LIRE(self.data,self.lgn,'PSSR_VAC')
+        self.DMTR    =LIRE(self.data,self.lgn,'DMTR')
